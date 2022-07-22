91dbff71ac2dffc611b5e5be31e44bec930fbefa
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-07-22 22:27:08 +0300

    swift temel bilgiler

---
+++ degiskenler.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+var benimSayim = 10 * 5
+20 + 10
+
+var ismim = "Ayberg"
+var soyIsmim = "Oguz"
+
+print(ismim + " " + soyIsmim) //yorum satiri
+
+let myNumber = 50 //sabit olusturma
+
+// myNumber = 30 yapamazsin
+
+let pi = 3.14
+
+ismim.uppercased()
+
+ismim.count
+
+var benimBoolean = true
+benimBoolean = false
+
+var doubleNumara1=80.0
+var doubleNumara2=3.0
+
+doubleNumara1 / doubleNumara2
+
+let benimSayim3 : Int8 = 32 // 8-16-32-64 bit
+
+let myString : String // defining tanimlama
+myString = String(benimSayim3)
+
+myString + "10"
+
+let intString = Int(myString)
+intString! + 20
+
+
+
+
+
+
+ 
+
+
+
