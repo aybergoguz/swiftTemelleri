e8b9c248c45b84a1f25d4044de3d6b3b70025b76
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-07-23 08:53:45 +0300

    diziler

--- kolleksiyonlar.playground/Contents.swift
+++ kolleksiyonlar.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var ilkDizimiz = ["Atil","Ayberg","Bulent","Atlas","Hulya"]
+for eleman in ilkDizimiz{
+    print(eleman)
+}
+
+ilkDizimiz[0]
+ilkDizimiz[1]
+
+var ikinciDizimiz=[10,20,30,40]
+ikinciDizimiz[1] * 5 / 10
+
+var karisikDizi = [100,200,"Ayberg",true,false] as [Any]
+ 
+var degisken = karisikDizi[2] as! String
+
+degisken.count
+
+karisikDizi.append("Ayse")
+
+karisikDizi.count
+karisikDizi[karisikDizi.count-1]
+karisikDizi.first
+karisikDizi.last
+
+var numaralarDizisi = [9,2,7,3,5,4,1,8,6]
+numaralarDizisi.sort()
+
+var harflerDizisi = ["z","d","a","k","b","y"]
+harflerDizisi.sort()
